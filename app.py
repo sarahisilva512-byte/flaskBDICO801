82864a09594743db11ca8c17fd6cd10496b11905
Sarahi Silva <sarahisilva512@gmail.com>
2026-04-17 11:08:44 -0600

    Agrego consultas

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ app = Flask(__name__)
 
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'instance', 'escuela.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config.from_object(DevelopmentConfig)
